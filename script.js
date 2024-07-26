58c84690eae171cf6ba6ba6667cc7a2e46eca84f
Inssa1234 <142233429+Inssa1234@users.noreply.github.com>
2024-07-26 15:42:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ window.addEventListener("scroll", (e) => {
         header.classList.remove('header');
     }
 });
+NProgress.start(); // Affiche la barre de progression
+
+window.onload = function() {
+  NProgress.done(); // Masque la barre de progression lorsque la page est complètement chargée
+};
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+      document.querySelector(
+        "body"
+      ).style.visibility = "hidden";
+      document.querySelector(
+        "#progress-bar"
+      ).style.visibility = "visible";
+    } else {
+      document.querySelector(
+        "#progress-bar"
+      ).style.display = "none";
+      document.querySelector(
+        "body"
+      ).style.visibility = "visible";
+    }
+  };
+  
+  let progress = 0;
+  const interval = setInterval(() => {
+    if (progress < 100) {
+      progress += 10;
+      document.querySelector('.progress-bar-fill').style.width = `${progress}%`;
+    } else {
+      clearInterval(interval);
+    }
+  }, 500);
+  
